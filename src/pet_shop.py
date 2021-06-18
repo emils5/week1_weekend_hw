f63fc430253192230e9b8381d1d84ccc52b9b0a6
Emil <emil.shiva@gmail.com>
2021-06-19 00:21:33 +0100

    first 8 tests passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -4,3 +4,25 @@ def get_pet_shop_name(pet_shop):
 
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
+
+def add_cash(pet_shop, cash_amount):
+    pet_shop["admin"]["total_cash"] = (pet_shop["admin"]["total_cash"] + 10)
+
+def remove_cash(pet_shop, cash_amount):
+    pet_shop["admin"]["total_cash"] = (pet_shop["admin"]["total_cash"] - 10)
+    
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, pets_sold):
+    pet_shop["admin"]["pets_sold"] = (pet_shop["admin"]["pets_sold"] + 2)
+
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed_to_find):
+    pets_matching= []
+    for pet in pet_shop["pets"]:
+        if breed_to_find == pet["breed"]:
+            pets_matching.append(pet["name"])
+    return pets_matching
