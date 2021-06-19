ce4f0236d908bfd11d7a3d25fc750ffc3c137b59
Emil <emil.shiva@gmail.com>
2021-06-19 14:16:42 +0100

    first 13 tests passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -6,7 +6,7 @@ def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
 def add_cash(pet_shop, cash_amount):
-    pet_shop["admin"]["total_cash"] = (pet_shop["admin"]["total_cash"] + 10)
+    pet_shop["admin"]["total_cash"] = (pet_shop["admin"]["total_cash"] + cash_amount)
 
 def remove_cash(pet_shop, cash_amount):
     pet_shop["admin"]["total_cash"] = (pet_shop["admin"]["total_cash"] - 10)
@@ -15,7 +15,7 @@ def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
 
 def increase_pets_sold(pet_shop, pets_sold):
-    pet_shop["admin"]["pets_sold"] = (pet_shop["admin"]["pets_sold"] + 2)
+    pet_shop["admin"]["pets_sold"] = (pet_shop["admin"]["pets_sold"] + pets_sold)
 
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
@@ -26,3 +26,37 @@ def get_pets_by_breed(pet_shop, breed_to_find):
         if breed_to_find == pet["breed"]:
             pets_matching.append(pet["name"])
     return pets_matching
+
+def get_pets_by_breed_non_found(pet_shop, breed_to_find):
+    pets_matching= []
+    for pet in pet_shop["pets"]:
+        if breed_to_find == pet["breed"]:
+            pets_matching.append(pet["name"])
+    return pets_matching
+
+def find_pet_by_name(pet_shop, pet_name):
+    for pet in pet_shop["pets"]:
+        if pet_name == pet["name"]:
+            return pet
+
+def find_pet_by_name_None(pet_shop, pet_name):
+    for pet in pet_shop["pets"]:
+        if pet_name == pet["name"]:
+            return pet
+        else: 
+            print("None")
+# returns None
+
+def remove_pet_by_name(pet_shop, pet_name):
+    for pet in pet_shop["pets"]:
+        if pet_name == pet["name"]:
+            pet_shop["pets"].remove(pet)
+
+def add_pet_to_stock(pet_shop, new_pet):
+    pet_shop["pets"].append(new_pet)
+    
+
+
+
+
+        
